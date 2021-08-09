1916c506125dace0a53557980751d6221d019b6b
Brandon Oleksik <brandon.oleksik@techelevator.com>
2021-08-09 12:58:40 -0400

    transfer pcs

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        //uses entiy framework to create the id similar to how sql creates id's.
+        [Key]
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+
+    }
+}
